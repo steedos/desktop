38494a8017ace9ec04f6ac8b7ea6e3f9e78446ab
chenzhipei <chenzhipei@hotoa.com>
2016-09-13 16:52:15 +0800

    修改在线编辑时无法关闭客户端，编辑完成后关闭

--- app/vendor/nw-boilerplate/background.js
+++ app/vendor/nw-boilerplate/background.js
@@ -41,15 +41,6 @@ cos.office_signal = function(signal){
     return signal;
 }
 
-cos.officeAlert = function(){
-    var local = window.navigator.language;
-    if (local == "zh-cn"){
-        alert("正在编辑office文档，请在编辑完成后，再关闭华炎云。");
-    }else{
-        alert("You are editing office document,please close Steedos after you have finished editing.");
-    }
-}
-
 // 重新载入时再次传入cos对象
 win.on("loaded", function(){
     win.window.cos = cos;
@@ -57,10 +48,7 @@ win.on("loaded", function(){
 
 //关闭华炎云时，将没有显示的主窗口关掉
 win.on("close",function(){
-    if(win.window){
-        if(cos.office_signal == "editing"){
-            cos.officeAlert();
-        }else{
+    if(win.window && (cos.office_signal != "editing")){
             win.close(true);
             globalWindow.close(true);
         }
