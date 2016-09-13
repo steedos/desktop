6d5f08afab21f36920b13cedaaa69cba9295c6c4
chenzhipei <chenzhipei@hotoa.com>
2016-09-13 16:43:41 +0800

    修改提示方法

--- app/vendor/nw-boilerplate/background.js
+++ app/vendor/nw-boilerplate/background.js
@@ -6,6 +6,8 @@ var gui = require('nw.gui');
 
 var globalWindow = gui.Window.get(); 
 
+var swal = require('sweetalert');
+
 var win = gui.Window.open('https://www.steedos.com/steedos/springboard/', {
     title:'Steedos',
     icon: 'icon.png',
@@ -17,7 +19,7 @@ var win = gui.Window.open('https://www.steedos.com/steedos/springboard/', {
     position: 'center'
 });
 
-cos_enabled_domain = ["127.0.0.1", "localhost", ".steedos.com", ".petrochina.com.cn"]
+cos_enabled_domain = ["127.0.0.1", "localhost", "192.168.0.189", ".steedos.com", ".petrochina.com.cn"]
 cos = {}
 cos.require = function (module){
     if (win.window && win.window.location)
@@ -34,6 +36,7 @@ cos.win_focus = function(){
     win.restore();//恢复最小化窗口
     win.focus();//获取焦点
 }
+
 cos.office_signal = function(signal){
     return signal;
 }
@@ -41,16 +44,15 @@ cos.office_signal = function(signal){
 cos.officeAlert = function(){
     var local = window.navigator.language;
     if (local == "zh-cn"){
-        swal("正在编辑office文档，请在编辑完成后，再关闭华炎云。");
+        alert("正在编辑office文档，请在编辑完成后，再关闭华炎云。");
     }else{
-        swal("You are editing office document,please close Steedos after you have finished editing.");
+        alert("You are editing office document,please close Steedos after you have finished editing.");
     }
 }
+
 // 重新载入时再次传入cos对象
 win.on("loaded", function(){
-    if (win.window){
-        win.window.cos = cos
-    }
+    win.window.cos = cos;
 })
 
 //关闭华炎云时，将没有显示的主窗口关掉
@@ -59,7 +61,7 @@ win.on("close",function(){
         if(cos.office_signal == "editing"){
             cos.officeAlert();
         }else{
-            win.close();
+            win.close(true);
             globalWindow.close(true);
         }
     }
